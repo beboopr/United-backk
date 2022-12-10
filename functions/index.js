3a7cb9bf00ab235e87bec823c0e95e7d9700ab2c
beboopr <linnets-router.09@icloud.com>
2022-12-10 16:00:26 -0500

    disabled Auth. currently on testing

--- functions/index.js
+++ functions/index.js
@@ -2,17 +2,17 @@ import { getAllItems,  getItem, createItems, updateItems } from "./src/routes.js
 import functions from "firebase-functions";
 import express from "express";
 import cors from "cors";
-import { userLogin, addNewUser, updateUser} from "./src/users.js";
-import { isUserReallyUser } from "./src/middleware.js";
+// import { userLogin, addNewUser, updateUser} from "./src/users.js";
+// import { isUserReallyUser } from "./src/middleware.js";
 
 
 const app = express();
 app.use(cors());
 app.use(express.json())
 
-app.patch('/users/:uid', isUserReallyUser ,updateUser)
-app.post('/login', userLogin)
-app.post('/users', addNewUser)
+// app.patch('/users/:uid', isUserReallyUser ,updateUser)
+// app.post('/login', userLogin)
+// app.post('/users', addNewUser)
 
 app.get('/items', getAllItems)
 app.get('/items/:uid', getItem)
